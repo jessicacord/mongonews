3a40739e15aa9f212cbc159bb489b97aa3caec61
jessicacord <jessicaleecord@gmail.com>
2018-04-25 23:15:59 -0400

    updated port for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 var logger = require("morgan");
 var exphbs = require("express-handlebars");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 app.use(express.static("public"));
